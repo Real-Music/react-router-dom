b06551c0372c4bcd36572e4ef045c803c7f78d06
mr-ray <fedjioraymond@gmail.com>
2024-06-27 19:05:09 +0100

    contact route

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root.js";
 import ErrorPage from "./error-page.js";
+import Contact from "./routes/contact.js";
 
 const router = createBrowserRouter([
   { path: "/", element: <Root />, errorElement: <ErrorPage /> },
+  { path: "contacts/:contactId", element: <Contact /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
