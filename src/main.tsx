7c5879687c39d240008dce6203f3c9b6527e2a05
mr-ray <fedjioraymond@gmail.com>
2024-06-27 18:52:17 +0100

    create root page

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
+import Root from "./routes/root.js";
 
-const router = createBrowserRouter([
-  { path: "/", element: <div>Hello World</div> },
-]);
+const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
