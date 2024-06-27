ca973b7a8b8eb2edf0bf4c8bfa76dd0ba07804da
mr-ray <fedjioraymond@gmail.com>
2024-06-27 19:23:17 +0100

    loader and actions

--- src/contacts.ts
+++ src/contacts.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import localforage from "localforage";
 import { matchSorter } from "match-sorter";
 import sortBy from "sort-by";
@@ -14,9 +17,9 @@ export async function getContacts(query) {
 
 export async function createContact() {
   await fakeNetwork();
-  let id = Math.random().toString(36).substring(2, 9);
-  let contact = { id, createdAt: Date.now() };
-  let contacts = await getContacts();
+  const id = Math.random().toString(36).substring(2, 9);
+  const contact = { id, createdAt: Date.now() };
+  const contacts = await getContacts();
   contacts.unshift(contact);
   await set(contacts);
   return contact;
@@ -24,15 +27,15 @@ export async function createContact() {
 
 export async function getContact(id) {
   await fakeNetwork(`contact:${id}`);
-  let contacts = await localforage.getItem("contacts");
-  let contact = contacts.find((contact) => contact.id === id);
+  const contacts = await localforage.getItem("contacts");
+  const contact = contacts.find((contact) => contact.id === id);
   return contact ?? null;
 }
 
 export async function updateContact(id, updates) {
   await fakeNetwork();
-  let contacts = await localforage.getItem("contacts");
-  let contact = contacts.find((contact) => contact.id === id);
+  const contacts = await localforage.getItem("contacts");
+  const contact = contacts.find((contact) => contact.id === id);
   if (!contact) throw new Error("No contact found for", id);
   Object.assign(contact, updates);
   await set(contacts);
@@ -40,8 +43,8 @@ export async function updateContact(id, updates) {
 }
 
 export async function deleteContact(id) {
-  let contacts = await localforage.getItem("contacts");
-  let index = contacts.findIndex((contact) => contact.id === id);
+  const contacts = await localforage.getItem("contacts");
+  const index = contacts.findIndex((contact) => contact.id === id);
   if (index > -1) {
     contacts.splice(index, 1);
     await set(contacts);
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
-import Root from "./routes/root.js";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root.js";
 import ErrorPage from "./error-page.js";
 import Contact from "./routes/contact.js";
 
@@ -11,6 +14,8 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    loader: rootLoader,
+    action: rootAction,
     errorElement: <ErrorPage />,
     children: [{ path: "contacts/:contactId", element: <Contact /> }],
   },
